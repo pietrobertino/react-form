6507966126428088b26618eef29a064254395852
pietrobertino <pietrobertino20@gmail.com>
2026-03-12 15:11:03 +0100

    complete milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
 
+const articles = [
+  "Guida al Coding",
+  "Design Minimalista",
+  "Futuro dell'AI",
+  "Sviluppo Agile",
+  "Smart Working"
+];
+
+
 
 function App() {
 
   return (
     <>
+      <div className="container">
+        <ul>
+          {articles.map(article => (
+            <li>{article}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
